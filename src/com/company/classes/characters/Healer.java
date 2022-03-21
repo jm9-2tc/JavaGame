796090ebccccefdf809d0a341bc0c0d44da3c149
unknown <nione>
2022-03-21 14:35:09 +0100

    first commit

---
+++ src/com/company/classes/characters/Healer.java
@@ -0,0 +1,40 @@
+package com.company.classes.characters;
+
+import com.company.classes.AttackType;
+import com.company.classes.CharacterClass;
+
+import java.awt.event.KeyEvent;
+
+public class Healer extends CharacterClass {
+    public Healer(String name) {
+        super();
+
+        this.setX(0);
+        this.setY(0);
+        this.setName(name);
+
+        setup();
+    }
+
+    public Healer(String name, int x, int y, int key1, int key2, int key3, int key4, int key5, int key6) {
+        super(key1, key2, key3, key4, key5, key6);
+
+        this.setX(x);
+        this.setY(y);
+        this.setName(name);
+
+        setup();
+    }
+
+    private void setup() {
+        this.setLevel(0);
+        this.setMaxHealthPoints(1000);
+        this.setHealthPoints(1000);
+        this.setManaPoints(200);
+        this.setMaxManaPoints(200);
+        this.setAttackType(AttackType.PHYSICAL);
+        this.setAttackAmount(5);
+
+        this.uploadImage("1.png", "2.png", "3.png");
+    }
+}
---
+++ src/com/company/classes/characters/Warrior.java
@@ -0,0 +1,41 @@
+package com.company.classes.characters;
+
+import com.company.classes.AttackType;
+import com.company.classes.CharacterClass;
+
+import java.awt.event.KeyEvent;
+
+public class Warrior extends CharacterClass {
+
+    public Warrior(String name) {
+        super();
+
+        this.setX(0);
+        this.setY(0);
+        this.setName(name);
+
+        setup();
+    }
+
+    public Warrior(String name, int x, int y, int key1, int key2, int key3, int key4, int key5, int key6) {
+        super(key1, key2, key3, key4, key5, key6);
+
+        this.setX(x);
+        this.setY(y);
+        this.setName(name);
+
+        setup();
+    }
+
+    private void setup() {
+        this.setLevel(0);
+        this.setMaxHealthPoints(1000);
+        this.setHealthPoints(1000);
+        this.setManaPoints(200);
+        this.setMaxManaPoints(200);
+        this.setAttackType(AttackType.PHYSICAL);
+        this.setAttackAmount(5);
+
+        this.uploadImage("1.png", "2.png", "3.png");
+    }
+}
