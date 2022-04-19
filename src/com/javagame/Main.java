8fbb1e17c87631adc4df2d78bc7acd256ef7a010
Jan Marmucki <jan.marmucki@hotmail.com>
2022-04-19 22:06:18 +0200

    Improved communication between classes.

--- src/com/javagame/Main.java
+++ src/com/javagame/Main.java
@@ -1,8 +1,17 @@
 package com.javagame;
 
+import com.javagame.game.GameEvents;
+import com.javagame.game.GameInstance;
+import com.javagame.gui.GamePanel;
+import com.javagame.gui.GameWindow;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        GameEvents events = new GameEvents();
+        GameInstance game = new GameInstance(events, null, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+
+        GamePanel panel = new GamePanel(events, game, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+        GameWindow window = new GameWindow(panel);
     }
 }
--- src/com/javagame/game/GameEvents.java
+++ src/com/javagame/game/GameEvents.java
@@ -3,19 +3,21 @@ package com.javagame.game;
 import com.javagame.game.player.Player;
 
 public class GameEvents implements Runnable {
-    private final GameInstance gameInstance;
+    private GameInstance gameInstance;
 
     private boolean keyboardDisabled = false;
 
     public long time = 0;
 
-    public GameEvents(GameInstance gameInstance) {
-        this.gameInstance = gameInstance;
-    }
-
     public void play() {
         time = System.currentTimeMillis();
     }
+
+
+    public void setGameInstance(GameInstance gameInstance) {
+        this.gameInstance = gameInstance;
+    }
+
 
     public void handleClickEvent(int x, int y) {
 
